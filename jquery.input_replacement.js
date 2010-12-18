3f0f814c0565fdaa1ccf1e00d14c8c6d0c4de6d9
Dana Woodman <dana@danawoodman.com>
2010-12-18 13:52:06 -0800

    Plugin can now handle input fields that already have a 'value' property.
    If the field has a 'value' then that will be shown. If the users changes the field to be empty, the default text will appear.
    
    Also added an example file ('example.html') that gives some demos of how to use the plugin with code snippets.

--- jquery.input_replacement.js
+++ jquery.input_replacement.js
@@ -4,7 +4,7 @@
     @description    A input text replacement that appends text to input fields 
                     and removes it on focus. Respects prepopulated fields.
     @author         Dana Woodman <dana@danawoodman.com>
-    @version        1.0
+    @version        1.1
     @license:       MIT
 
 ============================================================================= */
@@ -23,81 +23,94 @@
             // Build element specific options.
             obj.o = $.meta ? $.extend({}, opts, $this.data()) : opts;
             
-            // If field is empty, append text, classes, etc...
+            // Initialize the field_text variable that will store the 
+            // value of the field.
+            obj.o.field_text = '';
+            
+            // If the "text" option has been set, then apply it to the form 
+            // field.
+            if (obj.o.text) {
+                
+                obj.o.field_text = obj.o.text;
+                
+            } 
+            
+            // If the "text" option was not set in the options, then take 
+            // the "name" property of the field, capitilize it, and use it 
+            // instead.
+            else {
+                
+                // Get the name attribute of the field and capitalize the 
+                // first letter so it looks nice.
+                string = obj.attr('name');
+                string = string.charAt(0).toUpperCase() + string.slice(1);
+                
+                obj.o.field_text = string;
+            };
+            
+            // Set the value of the field if the field does not already have 
+            // a value.
             if (obj.val() == '') {
                 
-                // Define the field text to use in the 
-                obj.o.field_text = '';
-                
-                // If the "text" option has been set, then apply it to the form 
-                // field.
-                if (obj.o.text) {
-                    
-                    obj.o.field_text = obj.o.text;
-                    
-                } 
-                
-                // If the "text" option was not set in the options, then take 
-                // the "name" property of the field, capitilize it, and use it 
-                // instead.
-                else {
-                    
-                    // Get the name attribute of the field and capitalize the 
-                    // first letter.
-                    string = obj.attr('name');
-                    string = string.charAt(0).toUpperCase() + string.slice(1);
-                    
-                    obj.o.field_text = string;
-                };
-                
-                // Set the value of the field.
+                // Give the field the value of the field_text variable.
                 obj.val(obj.o.field_text);
-                
+
                 // Append the prefocus_class to the field if it is defined 
                 // in the options.
                 if (obj.o.prefocus_class) {
                     obj.addClass(obj.o.prefocus_class);
                 };
                 
-                // Focus on the input has occurred.
-                obj.bind('focus', function() {
-                    if (obj.val() == obj.o.field_text) {
-                        obj.val('');
-                    };
-                    if (obj.o.prefocus_class) {
-                        obj.removeClass(obj.o.prefocus_class);
-                    };
-                });
+            }
+            
+            // Focus on the input has occurred.
+            obj.bind('focus', function() {
                 
-                // Focus has been lost.
-                obj.bind('blur', function() {
-                    if (obj.val() == '') {
-                        obj.val(obj.o.field_text);
-                        if (obj.o.prefocus_class) {
-                            obj.addClass(obj.o.prefocus_class);
-                        };
-                    };
-                });
-                
-                // Clear out the values on reload so they arent loaded after refresh.
-                $(window).unload(function() {
-                   if (obj.val() == obj.o.field_text) {
-                       obj.val('');
-                   }; 
-                });
-                
-                // If nothing was entered, make sure the "text" is not submitted by removing it.
-                var form = obj.parents('form');
-                if (form) {
-                    form.each(function() {
-                        $(form).bind('submit', function() {
-                            if (obj.val() == obj.o.field_text) {
-                                obj.val('');
-                            };
-                        });
-                    });
+                // If the value of the field is the same as field_text then 
+                // clear it out so the user can enter content.
+                if (obj.val() == obj.o.field_text) {
+                    obj.val('');
                 };
                 
+                // If there is a prefocus_class, remove the class so the text 
+                // reverts to it's normal style.
+                if (obj.o.prefocus_class) {
+                    obj.removeClass(obj.o.prefocus_class);
+                };
+                
+            });
+            
+            // Focus has been lost.
+            obj.bind('blur', function() {
+                
+                // If the field has no value, populate it with the field_text 
+                // value and give it a prefocus_class if available.
+                if (obj.val() == '') {
+                    obj.val(obj.o.field_text);
+                    if (obj.o.prefocus_class) {
+                        obj.addClass(obj.o.prefocus_class);
+                    };
+                };
+                
+            });
+            
+            // Clear out the values on reload so they arent loaded after refresh.
+            $(window).unload(function() {
+               if (obj.val() == obj.o.field_text) {
+                   obj.val('');
+               }; 
+            });
+            
+            // If nothing was entered, make sure the "text" is not submitted by removing it.
+            var form = obj.parents('form');
+            if (form) {
+                form.each(function() {
+                    $(form).bind('submit', function() {
+                        if (obj.val() == obj.o.field_text) {
+                            obj.val('');
+                        };
+                    });
+                });
             };
             
         });
--- jquery.input_replacement.pack.js
+++ jquery.input_replacement.pack.js
@@ -4,12 +4,11 @@
     @description    A input text replacement that appends text to input fields 
                     and removes it on focus. Respects prepopulated fields.
     @author         Dana Woodman <dana@danawoodman.com>
-    @version        1.0
+    @version        1.1
     @license:       MIT
 
 ============================================================================= */
 
-// Packed with http://javascriptcompressor.com/ (with "Shrink variables" option)
+// Minified with http://jscompress.com/ (using Packer)
 
-*/
-eval(function(p,a,c,k,e,d){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--){d[e(c)]=k[c]||e(c)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('(6($){$.f.g=6(l){a b=$.j({},$.f.g.i,l);s $(c).h(6(){a 2=$(c);2.o=$.q?$.j({},b,$c.r()):b;4(2.3()==\'\'){2.o.5=\'\';4(2.o.e){2.o.5=2.o.e}t{8=2.m(\'n\');8=8.p(0).D()+8.B(1);2.o.5=8};2.3(2.o.5);4(2.o.7){2.k(2.o.7)};2.d(\'C\',6(){4(2.3()==2.o.5){2.3(\'\')};4(2.o.7){2.A(2.o.7)}});2.d(\'z\',6(){4(2.3()==\'\'){2.3(2.o.5);4(2.o.7){2.k(2.o.7)}}});$(u).w(6(){4(2.3()==2.o.5){2.3(\'\')}});a 9=2.x(\'9\');4(9){9.h(6(){$(9).d(\'y\',6(){4(2.3()==2.o.5){2.3(\'\')}})})}}})};$.f.g.i={e:\'\',7:\'\'}})(v);',40,40,'||obj|val|if|field_text|function|prefocus_class|string|form|var|opts|this|bind|text|fn|input_replacement|each|defaults|extend|addClass|options|attr|name||charAt|meta|data|return|else|window|jQuery|unload|parents|submit|blur|removeClass|slice|focus|toUpperCase'.split('|'),0,{}))
+eval(function(p,a,c,k,e,d){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--){d[e(c)]=k[c]||e(c)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('(6($){$.f.g=6(l){a b=$.j({},$.f.g.i,l);s $(c).h(6(){a 2=$(c);2.o=$.q?$.j({},b,$c.r()):b;2.o.5=\'\';4(2.o.e){2.o.5=2.o.e}t{8=2.m(\'n\');8=8.p(0).D()+8.B(1);2.o.5=8};4(2.3()==\'\'){2.3(2.o.5);4(2.o.7){2.k(2.o.7)}}2.d(\'C\',6(){4(2.3()==2.o.5){2.3(\'\')};4(2.o.7){2.A(2.o.7)}});2.d(\'z\',6(){4(2.3()==\'\'){2.3(2.o.5);4(2.o.7){2.k(2.o.7)}}});$(u).w(6(){4(2.3()==2.o.5){2.3(\'\')}});a 9=2.x(\'9\');4(9){9.h(6(){$(9).d(\'y\',6(){4(2.3()==2.o.5){2.3(\'\')}})})}})};$.f.g.i={e:\'\',7:\'\'}})(v);',40,40,'||obj|val|if|field_text|function|prefocus_class|string|form|var|opts|this|bind|text|fn|input_replacement|each|defaults|extend|addClass|options|attr|name||charAt|meta|data|return|else|window|jQuery|unload|parents|submit|blur|removeClass|slice|focus|toUpperCase'.split('|'),0,{}))
